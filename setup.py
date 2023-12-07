ee7dea5a604813e38dde54ef579fd0986d54288b
anthony.chou <anthony.chou@bluesg.com.sg>
2023-12-07 17:33:15 +0800

    Update the package name

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ install_requires = [
     "xgboost>=1.4.0",
     "numpy>=1.18.4",
     "scikit-learn>=0.22.2",
-    "pandas>=1.0.*",
+    "pandas",
     "joblib>=0.15.1",
-    "lifelines>=0.25.4",
+    "lifelines>=0.25.4"
 ]
 
 docs_packages = [
     "mkdocs>=1.1",
     "mkdocs-material>=4.6.3",
-    "mkdocstrings>=0.8.0",
+    "mkdocstrings>=0.8.0"
 ]
 
 dev_packages = [
@@ -20,11 +20,11 @@ dev_packages = [
     "flake8>=3.7.9",
     "pre-commit>=2.7.1",
     "pytest>=6.1.0",
-    "pytest-cov==2.10.1",
+    "pytest-cov==2.10.1"
 ] + docs_packages
 
 benchmark_packages = [
-    "pycox==0.2.1",
+    "pycox==0.2.1"
 ]
 
 all_packages = install_requires + dev_packages + benchmark_packages
@@ -33,7 +33,7 @@ with open("docs/index.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="xgbse",
+    name="xgbseanthony",
     version="0.2.3",
     author="Loft Data Science Team",
     author_email="bandits@loft.com.br",
@@ -42,11 +42,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     install_requires=install_requires,
-    extras_require={
-        "docs": docs_packages,
-        "dev": dev_packages,
-        "all": all_packages,
-    },
+    # extras_require={
+    #     "docs": docs_packages,
+    #     "dev": dev_packages,
+    #     "all": all_packages,
+    # },
     python_requires=">=3.7",
-    url="https://github.com/loft-br/xgboost-survival-embeddings",
+    # url="https://github.com/loft-br/xgboost-survival-embeddings",
 )
